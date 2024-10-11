b820bb2c1d729c8dc86cf672321664ea6b4a5103
BHALASAI <bhalasai.js@gmail.com>
2024-10-11 19:01:30 +0530

    lint fix

--- src/HelloAll.java
+++ src/HelloAll.java
@@ -1,5 +1,17 @@
+/**
+ * This class contains the main method which prints "Hello World".
+ */
 public class HelloAll {
-    public static void main(String ...args) {
+    private HelloAll() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    /**
+     * This is the main method which makes use of displayMessage method.
+     *
+     * @param args Unused.
+     */
+    public static void main(final String... args) {
         System.out.println("Hello World");
     }
 }
---
+++ src/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains classes related to the HelloAll application.
+ */
+package src.HelloAll;
